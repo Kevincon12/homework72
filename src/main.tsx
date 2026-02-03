ab64c098a87611c0f86de7a5eaac95d5dd97f8be
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-02-03 21:23:14 +0600

    Инициализировали проект

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from "react-redux";
+import {store} from "./app/store.ts";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </StrictMode>
+    </Provider>
+)
